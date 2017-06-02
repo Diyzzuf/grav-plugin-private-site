430bf7b2d0656663d9f7b46fca9e789828cb234c
Diyzzuf <diyzzuf@gmail.com>
2017-06-03 00:12:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Private Site Plugin  ( ON DEVELOPMENT )
+
+
+The **Private Site** Plugin is for [Grav CMS](http://github.com/getgrav/grav). It provide an authentification form to keep private your Grav site or part of it.
+
+## Installation
+
+Installing the Private Site plugin can be done in one of two ways. The GPM (Grav Package Manager) installation method enables you to quickly and easily install the plugin with a simple terminal command, while the manual method enables you to do so via a zip file.
+
+### GPM Installation (Preferred)
+
+The simplest way to install this plugin is via the [Grav Package Manager (GPM)](http://learn.getgrav.org/advanced/grav-gpm) through your system's terminal (also called the command line).  From the root of your Grav install type:
+
+    bin/gpm install private-site
+
+This will install the Private Site plugin into your `/user/plugins` directory within Grav. Its files can be found under `/your/site/grav/user/plugins/private-site`.
+
+### Manual Installation
+
+To install this plugin, just download the zip version of this repository and unzip it under `/your/site/grav/user/plugins`. Then, rename the folder to `private-site`. You can find these files on [GitHub](https://github.com/di-yzzuf/grav-plugin-private-site) or via [GetGrav.org](http://getgrav.org/downloads/plugins#extras).
+
+You should now have all the plugin files under
+
+    /your/site/grav/user/plugins/private-site
+	
+> NOTE: This plugin is a modular component for Grav which requires [Grav](http://github.com/getgrav/grav) and the [Error](https://github.com/getgrav/grav-plugin-error) and [Problems](https://github.com/getgrav/grav-plugin-problems) to operate.
+
+## Configuration
+
+Before configuring this plugin, you should copy the `user/plugins/private-site/private-site.yaml` to `user/config/plugins/private-site.yaml` and only edit that copy.
+
+Here is the default configuration and an explanation of available options:
+
+```yaml
+enabled: true
+```
+
+## Usage
+
+**Describe how to use the plugin.**
+
+## Credits
+
+**Did you incorporate third-party code? Want to thank somebody?**
+
+## To Do
+
+- [ ] Future plans, if any
+
---
+++ private-site.php
@@ -0,0 +1,123 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use Grav\Common\Page;
+use Grav\Common\Data\Blueprints;
+use RocketTheme\Toolbox\Event\Event;
+
+/**
+ * Class PrivateSitePlugin
+ * @package Grav\Plugin
+ */
+class PrivateSitePlugin extends Plugin
+{
+
+    /**
+     * @var
+     */
+    protected $_privateConfig;
+
+
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents()
+    {
+        // Check for required plugins
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+
+        if (!$this->grav['config']->get('plugins.login.enabled') || !$this->grav['config']->get('plugins.form.enabled') || !$this->grav['config']->get('plugins.email.enabled')) {
+            throw new \RuntimeException('One of the required plugins is missing or not enabled');
+        }
+        
+        if ($this->isAdmin()) {
+            // Enable the main event we are interested in
+            $this->enable([
+                'onBlueprintCreated' => ['onBlueprintCreated', 0],
+                'onGetPageTemplates' => ['onGetPageTemplates', 0]
+            ]);
+        } else {
+            // Enable the main event we are interested in
+            $this->_privateConfig = $this->grav['config']->get('plugins.private-site', null);
+            $this->enable([
+                'onPageInitialized' => ['onPageInitialized', 1000]
+            ]);
+            
+        }
+
+    }
+
+    /**
+     * Check if site/page is private
+     */
+    public function onPageInitialized()
+    {
+        if( isset($this->grav['page']->header()->access) && isset($this->grav['page']->header()->access['site.login'] )){
+            return;
+        }
+
+        switch ($this->_privateConfig['private_site']) {
+            case true:
+            default:
+                if ( isset($this->grav['page']->header()->access) && isset($this->grav['page']->header()->access['site.login']) ) {
+                    if ($this->grav['page']->header()->access['site.login'] != true) {
+                        $this->grav['page']->modifyHeader('access', array('site.login' => true));
+                    }
+                } else {
+                    $this->grav['page']->modifyHeader('access', array('site.login' => true));
+                }
+                break;
+            
+            case false:
+                if ( isset($this->grav['page']->header()->private) && $this->grav['page']->header()->private == true ){
+                    $this->grav['page']->modifyHeader('access', array('site.login' => true));
+                }
+                break;
+        }
+
+    }
+
+    /**
+     * Extend Admin Page Blueprints
+     */
+    public function onBlueprintCreated(Event $event)
+    {
+        static $inEvent = false;
+        /** @var Data\Blueprint $blueprint */
+        $blueprint = $event['blueprint'];
+        if (!$inEvent && $blueprint->get('form/fields/tabs', null, '/')) {
+            $inEvent = true;
+            $blueprints = new Blueprints(__DIR__ . '/blueprints/');
+            $extends = $blueprints->get('private');
+            $blueprint->extend($extends, true);
+            $inEvent = false;
+        }
+    }
+
+    /**
+     * Add page template types.
+     */
+    public function onGetPageTemplates(Event $event){
+        /** @var Types $types */
+        $types = $event->types;
+        $types->register('private');
+    }
+
+}
